4439e826252abb317e3eb8e9a8b8b6244309cf85
Zayn <zaynhallal@gmail.com>
2024-09-01 09:20:27 -0400

    removed unessacary stuff.

--- Fusion-CS2/Source/Entities/Entities.hpp
+++ Fusion-CS2/Source/Entities/Entities.hpp
@@ -37,28 +37,4 @@ namespace Entities {
 
     void UpdateVisuals();
     void UpdateAimbot();
-
-    template <typename ReadType>
-    BOOL Read(uintptr_t Address, ReadType& Value, size_t Size)
-    {
-        if (Address == 0 || Address < 0xAA70AA8 || Address > 0x7FFD5DC070000)
-            return false;
-
-        MEMORY_BASIC_INFORMATION mbi;
-        if (VirtualQuery(reinterpret_cast<LPCVOID>(Address), &mbi, sizeof(mbi)) == 0)
-            return false;
-
-        if (mbi.State != MEM_COMMIT)
-            return false;
-
-        if ((mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) || !(mbi.Protect & (PAGE_READONLY | PAGE_READWRITE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE)))
-            return false;
-
-        if (Size > (static_cast<size_t>(mbi.RegionSize) - (Address - reinterpret_cast<uintptr_t>(mbi.BaseAddress))))
-            return false;
-
-        memcpy(&Value, reinterpret_cast<void*>(Address), Size);
-
-        return true;
-    }
 }
